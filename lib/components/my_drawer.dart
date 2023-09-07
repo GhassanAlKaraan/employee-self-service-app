e43646fbe0b2bc4c055f8e738e5e2a5b346303bf
GhassanAlKaraan <ghass.karaan@gmail.com>
2023-09-07 17:18:49 +0300

    fixed Drawer

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -14,58 +14,73 @@ class MyDrawer extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
+          const DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.red[900],
+              //color: Colors.red[900],
+              color: Colors.white,
             ),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Image(
+              image: AssetImage('assets/images/logos/gtsLogo.png'),
+            ),
+            // child: const Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     CircleAvatar(
+            //       radius: 40,
+            //       backgroundImage: AssetImage('assets/images/logos/apple.png'),
+            //     ),
+            //     SizedBox(height: 10),
+            //     Text(
+            //       'John Doe',
+            //       style: TextStyle(
+            //         color: Colors.white,
+            //         fontSize: 20,
+            //       ),
+            //     ),
+            //   ],
+            // ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Wrap(
+              runSpacing: 12,
               children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: AssetImage('assets/images/logos/google.png'),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  onTap: () {
+                    // Handle the tap event for Home
+                  },
                 ),
-                SizedBox(height: 10),
-                Text(
-                  'John Doe',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () {
+                    // Handle the tap event for Settings
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.chat),
+                  title: const Text('Chat'),
+                  onTap: () {
+                    // Handle the tap event for Chat
+                  },
+                ),
+                Divider(
+                  thickness: 1.0,
+                  color: Colors.grey[500],
+                ),
+                //See all users
+                ListTile(
+                  leading: const Icon(Icons.group),
+                  title: const Text('All Users'),
+                  onTap: () {
+                    utility.goToPage(context, const AllUsersPage());
+                  },
                 ),
               ],
             ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              // Handle the tap event for Home
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Settings'),
-            onTap: () {
-              // Handle the tap event for Settings
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.chat),
-            title: const Text('Chat'),
-            onTap: () {
-              // Handle the tap event for Chat
-            },
-          ),
-          Divider(thickness: 1.0,color: Colors.grey[400],),
-          //See all users
-          ListTile(
-            leading: const Icon(Icons.group),
-            title: const Text('All Users'),
-            onTap: () {
-              utility.goToPage(context, const AllUsersPage());
-            },
-          ),
+          )
         ],
       ),
     );
